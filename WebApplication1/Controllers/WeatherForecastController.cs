465921f1e5a4557f2c312a8078f992dc3bcded9e
mohamed.agour <mohamed.agour@flairstech.com>
2022-11-23 20:02:32 +0200

    ads

--- WebApplication1/Controllers/WeatherForecastController.cs
+++ WebApplication1/Controllers/WeatherForecastController.cs
@@ -1,16 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+﻿
+using Microsoft.AspNetCore.Mvc;
 using Spire.Doc;
 using Spire.Doc.Documents;
-using Spire.Doc.Fields;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Reflection;
-using System.Threading.Tasks;
-using System.Xml.Linq;
 using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
@@ -57,6 +51,7 @@ namespace WebApplication1.Controllers
             Header.Add("AssessmentForAdmin", "Assessment For Admin");
             Header.Add("NetInvestmentReturn", "Net Investment Return");
             Header.Add("GrantsFromFund", "Grants From Fund");
+
             Header.Add("TransfersToCharitableGiftFund", "Transfers To Charitable GiftFund");
             Header.Add("ClosingValue", "Closing Value");
             Header.Add("OpeningBalanceGrantMoney", "Opening Balance Grant Money");
@@ -79,6 +74,8 @@ namespace WebApplication1.Controllers
                 dataRow.Cells[0].CellFormat.BackColor = Color.LightSeaGreen;
                 Paragraph par2 = dataRow.Cells[1].AddParagraph();
                 var objValue = report.GetType().GetProperty(kvp.Key).GetValue(report, null);
+                if (objValue is null)
+                    objValue = string.Empty;
                 par.AppendText(kvp.Value);
                 par2.AppendText(objValue.ToString());
                 par.Format.HorizontalAlignment = HorizontalAlignment.Left;
